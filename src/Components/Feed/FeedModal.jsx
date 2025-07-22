676e58981c7c7ceed486f49bff215bbaf623d5c0
Alan Siqueira <alan.siqueira.25782@aluno.facape.br>
2025-07-21 23:28:27 -0300

    feat: add feed modal

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-
-const FeedModal = () => {
+import React from "react";
+import styles from "./FeedModal.module.css";
+import useFetch from "../../Hooks/useFetch";
+import { PHOTO_GET } from "../../api";
+import Error from "../Help/Error";
+import Loading from "../Help/Loading";
+import PhotoContent from "../Photo/PhotoContent";
+const FeedModal = ({ photo, setModalPhoto }) => {
+  const { data, error, loading, request } = useFetch();
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+  function handleOutsideClick(event){
+    if(event.target === event.currentTarget) setModalPhoto(null);
+  }
   return (
-    <div>
-      
+    <div className={styles.modal} onClick={handleOutsideClick}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
     </div>
-  )
-}
+  );
+};
 
-export default FeedModal
+export default FeedModal;
