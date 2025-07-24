d3419e56aa2d80647966705fa05eba41dfd51954
Alan Siqueira <alan.siqueira.25782@aluno.facape.br>
2025-07-24 11:25:44 -0300

    feat: add inifinite scroll

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./Components/Login/Login";
 import { UserStorage } from "./UserContext";
 import User from "./Components/User/User";
 import ProtectedRouter from "./Components/Help/ProtectedRouter";
+import Photo from "./Components/Photo/Photo";
 
 const App = () => {
   return (
@@ -25,6 +26,10 @@ const App = () => {
                   <User />
                 </ProtectedRouter>
               }
+            />
+            <Route
+            path="foto/:id" 
+            element={<Photo />}
             />
           </Routes>
           <Footer />
